e8b9333b10254bf14c4061f6f1ed2111e3df5f70
cimermanGregor <cimerman.gregor@gmail.com>
2014-01-09 20:13:22 +0100

    Dodan class VirtualInstrument TODO: serializacija objekov

--- VirtualSpeaker.py
+++ VirtualSpeaker.py
@@ -1,3 +1,6 @@
+from setuptools.command.build_ext import if_dl
+from numpy.core.multiarray import zeros
+
 __author__ = 'blaz'
 
 import sys
@@ -9,6 +12,67 @@ import sys
 import pybonjour
 import settings
 import socket
+import pygame
+
+class VirtualInstrument():
+    """
+    Class for playing virtual instrument bonded via socket
+    """
+    """ midi instrument codes
+    0   GRAND_PIANO
+    19  CHURCH_ORGAN
+    37  SLAP_BASS_1
+    41  VIOLIN
+    115 STEEL_DRUMS
+    """
+
+    instrument = 0
+    hostname = ""
+    midiDevice = 0
+    midi_output = None
+    keyPressed = [False for i in range(127)]
+    def __init__(self, hostname, instrument="1", midiDevice=None):
+        try:
+            instrument =  int(instrument)
+        except Exception:
+            print "Instrument ni pravilnega tipa"
+            raise Exception
+        self.instrument = instrument
+        self.hostname = hostname
+        if midiDevice is None:
+            self.midiDevice = pygame.midi.get_default_input_id()
+        else:
+            self.midiDevice = midiDevice
+        pygame.init()
+        self.midi_out = pygame.midi.Output(self.midiPort, 0)
+        try:
+            self.midi_out.set_instument(self.instrument)
+        except Exception:
+            print "Prislo je do napake"
+
+    def processData(self, data):
+        if data == "r":
+            # Refresh playing notes
+            self.refreshNotes()
+        elif data == "off":
+            keyPressed = [False for i in range(127)]
+            self.refreshNotes()
+            return False
+        else:
+            try:
+                nota = int(data)
+                self.keyPressed[nota] != self.keyPressed[nota]
+            except Exception:
+                print "Prislo je do napake pri sprejemu podatkov - podatki niso pravilne oblike"
+        self.refreshNotes()
+
+
+    def refreshNotes(self):
+        for i in range(len(self.keyPressed)):
+            if self.keyPressed[i]:
+                # note velocity is fixed
+                self.midi_out.note_on(i,127)
+            else: self.midi_out.note_off(i,127)
 
 class AdvertiseService(threading.Thread):
     """
@@ -78,6 +142,12 @@ print 'Socket bind complete'
 
 print "Waiting on port:", port
 
+#Check for connection loss and remove virtual instrument accordingly
+longtimeNoSee = list()
+
+#Virtual instrument list
+virtualInstruments = dict()
+
 """
 Loop forever
 """
@@ -86,6 +156,12 @@ while(1):
     #Read from socket
     data, addr = s.recvfrom(1024)
     print addr, ": ", data
+    #Virtual instrument missing from client list
+    if addr not in virtualInstruments.keys():
+        virtualInstruments.setdefault(addr, VirtualInstrument(hostname=addr, data))
+    else:
+        if not virtualInstruments[addr].processData(data):
+            del virtualInstruments[addr]
 
 s.close()
 
