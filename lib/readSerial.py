b11d756a5d35d003d4ddcf74bb40de1c945fcc35
cimermanGregor <cimerman.gregor@gmail.com>
2014-01-10 11:02:31 +0100

    Python reading serial port and printing output

--- lib/readSerial.py
+++ lib/readSerial.py
@@ -1,5 +1,12 @@
 import serial
+#sudo apt-get install python-serial
 
-ser = serial.Serial('/dev/tty.usbmodem1421', 9600)
+#Blaz's MBP
+#ser = serial.Serial('/dev/tty.usbmodem1421', 9600)
+#Gregor's HP and RaspberryPi
+ser = serial.Serial('/dev/ttyACM0', 9600)
 while True:
-	print ser.readline()
+	#print ser.readline()
+    line = ser.readline()
+    line = line.split('\t')
+    print line
